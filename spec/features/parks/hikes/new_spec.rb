97689bb9e60d9b718400a192190609cc234c5529
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-05-18 08:42:50 -0600

    Test: new parks Hike creation

--- spec/features/parks/hikes/new_spec.rb
+++ spec/features/parks/hikes/new_spec.rb
@@ -1,21 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe 'New Hike' do
-  describe 'When I visit the new hike form by clicking a link in the park hikes index'
-    it "I can create a new hike for that park" do
+RSpec.describe 'new parks Hike creation' do
+
+  let!(:zion) { Park.create!(name: 'Zion National Park', location: 'Utah', national_park_pass: true, fee: 20.00) }
+
+  describe 'When I visit the new hike form by clicking a link in the park hikes index' do
+    it "I can create a new hike for that park" do # >>>>>>>>>> USER STORY 13, ERROR <<<<<<<<<<<<<<<<<<
       visit "/parks/#{zion.id}/hikes"
 
       click_link('Create Hike')
 
-      expect(current_path).to eq("/parks/#{zion.id}/hikes/new")
+    expect(current_path).to eq("/parks/#{zion.id}/hikes/new")
+
       fill_in(:name, with: 'New test hike')
       fill_in(:length_miles, with: 'Montana')
-      fill_in(:park_id, with: 'zion.id')
       fill_in(:open, with: 'true')
 
       click_on('Create Hike')
 
-      expect(current_page).to eq("/parks/#{zion.id}/hikes")
+      expect(current_path).to eq("/parks/#{zion.id}/hikes")
       expect(page).to have_content('New test hike')
       expect(page).to_not have_content('Glacier National Park')
     end
